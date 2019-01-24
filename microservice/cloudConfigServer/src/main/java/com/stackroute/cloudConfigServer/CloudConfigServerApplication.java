709dde3ca1f7cab2d5c23db5a8fba1516262fa22
nilamaj1 <nilanjanmaji08@gmail.com>
2019-01-24 15:38:35 +0530

    inittial commit

---
+++ microservice/cloudConfigServer/src/main/java/com/stackroute/cloudConfigServer/CloudConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.stackroute.cloudConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerApplication.class, args);
+	}
+
+}
+
---
+++ microservice/cloudConfigServer/src/test/java/com/stackroute/cloudConfigServer/CloudConfigServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.stackroute.cloudConfigServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
